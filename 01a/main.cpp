ebe2c65b9de80c36e67bb09861cd1abbce8cd7a7
ForgotMyCode <nespamujtady@gmail.com>
2021-12-01 13:47:00 +0100

    01a

--- 01a/main.cpp
+++ 01a/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 int main(int argc, char** argv) {
+
+	int lastDepth = numeric_limits<int>::max();
+	int currentDepth;
+	int totalIncreases = 0;
+
+	cin >> currentDepth;
+	while(cin.good()) {
+		if(currentDepth > lastDepth) {
+			++totalIncreases;
+		}
+
+		lastDepth = currentDepth;
+
+		cin >> currentDepth;
+	}
+
+	cout << totalIncreases;
 
 	return 0;
 }
